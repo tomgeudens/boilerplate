a585c1ddec8e093d01b1e0b8188e5a8ef318b8b0
Tom Geudens <tomgeudens@users.noreply.github.com>
2025-01-21 13:19:41 +0100

    initial upload

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+// const webpack = require('webpack')
+const path = require('path')
+
+module.exports = {
+  mode: 'none',
+  entry: './src/js/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist/assets'),
+    filename: 'bundle.js'
+  },
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, 'dist')
+    },
+    devMiddleware: {
+      publicPath: '/assets/'
+    }
+  },
+  module: {
+    rules: [{
+      test: /\.js$/, // look for all files that end in .js
+      exclude: /node_modules|dist/, // except when it's in the node_modules or dist folder
+      use: {
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/preset-env']
+        }
+      }
+    }]
+  }
+};
